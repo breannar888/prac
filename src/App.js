d5dcf659f0b64bfa8fa4d80157fe24a759d3e5a5
breannar888 <67029835+breannar888@users.noreply.github.com>
2020-06-21 08:37:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
   
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +13,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }

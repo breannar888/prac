9544197e55b26c1995b3ef09b8a8493a7ef35aec
breannar888 <67029835+breannar888@users.noreply.github.com>
2020-06-21 08:39:09 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
-<html>
-    <div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-    </div>
-</html>
+<p> hi </p>
 
 
 

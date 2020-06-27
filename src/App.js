52294b3c65209fed336cf14ac16d1f2033299055
breannar888 <67029835+breannar888@users.noreply.github.com>
2020-06-27 08:19:11 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -4,22 +4,46 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<nav>
+			<ul>
+				<li class="current-menu-item"> <a href="">Home</a> </li>
+				<li> <a href="">About</a></li>
+				<li> <a href="">What is...</a>
+					<ul>
+						<li><a href="">ACAB</a></li>
+						<li><a href="">Defund the Police</a></li>
+						<li><a href="">Blue Wall of Silence</a></li>
+						<li><a href="">Activist Burnout</a></li>
+					</ul>
+				</li>
+				<li> <a href="">Common Arguments and How to Respond</a>
+					<ul>
+						<li><a href="">"Protests Are Violent"</a></li>
+						<li><a href="">"Looting is Too Much"</a></li>
+						<li><a href="">"Protests Don't Work"</a></li>
+						<li><a href="">"Everyone is Treated Equal By Police"</a></li>
+						<li><a href="">"Black People Commit More Crimes"</a></li>
+					</ul>
+				</li>
+				<li> <a href="">Primary Sources</a>
+					<ul>
+						<li><a href="">Videos and Photos of Police Brutality</a></li>
+						<li><a href="">Black Sentencing vs White Sentencing</a></li>
+					</ul>
+				</li>
+				<li> <a href="">How to Support</a>
+					<ul>
+						<li><a href="">Donate</a></li>
+						<li><a href="">Protest</a></li>
+						<li><a href="">Watch</a></li>
+						<li><a href="">Share</a></li>
+					</ul>
+				</li>
+				<li> <a href="">Say Their Names</a></li>
+				<li> <a href="">Mental Health</a></li>
+				<li> <a href="">Contacts</a></li>
+			</ul>
+		</nav>
   );
 }
 
